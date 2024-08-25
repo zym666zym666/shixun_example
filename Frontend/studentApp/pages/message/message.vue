fe5cafb6f1ee86dc03c9ae0fa7d889da246dd61e
CQU20222017 <158797898+CQU20222017@users.noreply.github.com>
2024-08-25 19:59:39 +0800

    2.0.1

---
+++ Frontend/studentApp/pages/message/message.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="content">
+		<view class="header">
+			<text>我的消息</text>
+		</view>
+		<view class="message_box">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.content{
+	display: flex;
+	flex-wrap: wrap;
+}
+.header{
+	width: 90%;
+	margin-left: 5%;
+	height: 100rpx;
+	border:solid #dfdfdf 1rpx;
+	border-radius: 20rpx;
+	background: #acacac;
+}
+.header text{
+	font-size: 50rpx;
+	margin-left: 225rpx;
+	line-height: 100rpx;
+}
+.message_box{
+	width: 95%;
+	height:150rpx;
+	margin-left: 2.5%;
+	margin-top: 10rpx;
+	border-bottom: solid #d4d4d4 1rpx;
+	
+}
+</style>
